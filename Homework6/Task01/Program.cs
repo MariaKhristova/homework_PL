b1428021632271b16a108f742c2b3dbcc182eb3a
Vitaly Kanunikov <y0zh@live.ru>
2023-11-06 17:09:00 +0300

    Added HM6

---
+++ Homework6/Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2-> 2
+//- 1, -7, 567, 89, 223-> 3
+
+var positiveNumbers = 0;
+
+void InputNumber()
+{
+    Console.WriteLine("Введите число: ");
+    string numstr = Console.ReadLine();
+    if (numstr == "")
+        return;
+    int num = Convert.ToInt32(numstr);
+    if (num > 0)
+        positiveNumbers++;
+    InputNumber();
+}
+
+InputNumber();
+Console.WriteLine("Чисел больше нуля: " + positiveNumbers);
