5101c758f7088f1e8a8c39dc03cda82ac61e47fe
Vitaly Kanunikov <y0zh@live.ru>
2023-11-12 12:45:58 +0300

    Added HM9

---
+++ Homework9/Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+var m = 4;
+var n = 8;
+
+int Summ(int i, int max)
+{
+    var summ = i;
+    if (i < max)
+        summ += Summ(i + 1, max);
+    return summ;
+}
+
+var sum = Summ(m, n);
+Console.WriteLine(sum);
---
+++ Homework9/Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+var m = 3;
+var n = 2;
+
+int Akkerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+
+    if (m == 0)
+        return Akkerman(n - 1, 1);
+
+    return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+var result = Akkerman(m, n);
+Console.WriteLine(result);
